9ebd07f95c03245fa068f1c568781cdaa410633a
public.karliner@googlemail.com <sven@karliner.de>
2015-11-19 14:06:19 +0100

    League of Legends extension added to remove code from league.page.php

--- extensions/pages/league.page.php
+++ extensions/pages/league.page.php
@@ -1,22 +1,12 @@
 <?php
 if (\components\input\Input::hasGET('summoner')):
-    $api_key = \components\configuration\ConfigurationFactory::getInstance()->get('riot_api_key');
     $summoner_name = \components\input\Input::getGET('summoner');
-    $summoner_name_plain = strtolower(str_replace(' ', '', $summoner_name));
     $region = \components\input\Input::getGET('region');
-    $summoner_url = "https://{$region}.api.pvp.net/api/lol/{$region}/v1.4/summoner/by-name/{$summoner_name_plain}?api_key={$api_key}";
-    $summoner_json_string = \components\utilites\CURL::url_get_contents($summoner_url);
-    $summoner_json = components\utilites\JSON::create($summoner_json_string);
-    $summoner_id = $summoner_json->{$summoner_name_plain}->{'id'};
-    $summaries_url = "https://{$region}.api.pvp.net/api/lol/{$region}/v1.3/stats/by-summoner/{$summoner_id}/summary?season=SEASON2015&api_key={$api_key}";
-    $summaries_json_string = \components\utilites\CURL::url_get_contents($summaries_url);
-    $summaries_json = components\utilites\JSON::create($summaries_json_string);
-    $recent_games_url = "https://{$region}.api.pvp.net/api/lol/{$region}/v1.3/game/by-summoner/{$summoner_id}/recent?api_key={$api_key}";
-    $recent_games_json_string = \components\utilites\CURL::url_get_contents($recent_games_url);
-    $recent_games_json = components\utilites\JSON::create($recent_games_json_string);
+    $summoner = new extensions\league\Summoner($summoner_name, $region);
+    $summoner->getGames();
     ?>
     <div class="page-header">
-        <h3><?php echo $summoner_name; ?></h3>
+        <h3><?php echo $summoner->getInfo('name'); ?></h3>
     </div>
     <div class="panel-group" id="summoner" role="tablist" aria-multiselectable="false">
         <div class="panel panel-default" id="summoner_data">
@@ -27,36 +17,32 @@ if (\components\input\Input::hasGET('summoner')):
                     </a>
                 </h3>
             </div>
-            <div id="stats" class="panel-collapse collapse in" role="tabpanel">
+            <div id="stats" class="panel-collapse toggle-group collapse in" role="tabpanel">
                 <?php
-                foreach ($summaries_json->{'playerStatSummaries'} as $summary):
-                    ?>
-                    <a class="block" href="#<?php echo $summary->{'playerStatSummaryType'}; ?>" aria-expanded="false" aria-controls="<?php echo $summary->{'playerStatSummaryType'}; ?>" role="button"  data-toggle="collapse" data-parent="#parent_<?php echo $summary->{'playerStatSummaryType'}; ?>">
-                        <?php echo components\language\Language::translate($summary->{'playerStatSummaryType'}); ?>
-                    </a>
-                    <div  id="<?php echo $summary->{'playerStatSummaryType'}; ?>" class="collapse">
-                        <table class="table table-bordered table-condensed table-league-data">
-                            <thead>
-                                <tr>
-                                    <th><?php echo \components\language\Language::translate('table_key'); ?></th>
-                                    <th><?php echo \components\language\Language::translate('table_value'); ?></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                foreach ($summary->{'aggregatedStats'} as $key => $value):
-                                    ?>
-                                    <tr>
-                                        <td><?php echo components\language\Language::translate($key); ?></td>
-                                        <td><?php echo $value; ?></td>
-                                    </tr>
+                foreach ($summoner->getStats('playerStatSummaries') as $summary):
+                    if (!empty((array)$summary->{'aggregatedStats'})) :
+                        ?>
+                        <a class="block toggle" href="#<?php echo $summary->{'playerStatSummaryType'}; ?>" aria-expanded="false" aria-controls="<?php echo $summary->{'playerStatSummaryType'}; ?>" role="button"  data-toggle="collapse" data-parent="#parent_<?php echo $summary->{'playerStatSummaryType'}; ?>">
+                            <?php echo components\language\Language::translate($summary->{'playerStatSummaryType'}); ?>
+                        </a>
+                        <div  id="<?php echo $summary->{'playerStatSummaryType'}; ?>" class="collapse">
+                            <table class="table table-bordered table-condensed table-league-data" data-type="bar">
+                                <tbody>
                                     <?php
-                                endforeach;
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>
-                    <?php
+                                    foreach ($summary->{'aggregatedStats'} as $key => $value):
+                                        ?>
+                                        <tr>
+                                            <td><?php echo components\language\Language::translate($key); ?></td>
+                                            <td><?php echo $value; ?></td>
+                                        </tr>
+                                        <?php
+                                    endforeach;
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <?php
+                    endif;
                 endforeach;
                 ?>
             </div>
@@ -70,7 +56,7 @@ if (\components\input\Input::hasGET('summoner')):
                 </h3>
             </div>
             <div id="recent" class="panel-collapse collapse" role="tabpanel" aria-labelledby="recent_heading">
-                <?php foreach ($recent_games_json->{'games'} as $game): ?>
+                <?php foreach ($summoner->getGames() as $game): ?>
                     <table class="table table-bordered table-condensed table-league-data" id="<?php echo $game->{'gameId'}; ?>">
                         <caption><?php echo $game->{'gameMode'}; ?></caption>
                         <thead>
